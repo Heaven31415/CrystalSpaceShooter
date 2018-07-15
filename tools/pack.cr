6e43e93d9c3e5cb59a783b43aa503b6bf5b437bc
Heaven31415 <Heaven31415@gmail.com>
2018-07-15 10:07:33 +0200

    Move autogenerated enums to data folder.

--- tools/pack.cr
+++ tools/pack.cr
@@ -8,15 +8,15 @@ textures_path = "resources/textures"
 
 packed_fonts = PackedResources(SF::Font).new(fonts_path)
 packed_fonts.save("data/fonts.data")
-packed_fonts.build_enum("common/fonts.cr", "Fonts")
+packed_fonts.build_enum("data/fonts.cr", "Fonts")
 
 packed_sounds = PackedResources(SF::SoundBuffer).new(sounds_path)
 packed_sounds.save("data/sounds.data")
-packed_sounds.build_enum("common/sounds.cr", "Sounds")
+packed_sounds.build_enum("data/sounds.cr", "Sounds")
 
 packed_textures = PackedResources(SF::Texture).new(textures_path)
 packed_textures.save("data/textures.data")
-packed_textures.build_enum("common/textures.cr", "Textures")
+packed_textures.build_enum("data/textures.cr", "Textures")
 
 
 

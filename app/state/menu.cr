7528b40e63e8c2001be11e09ca36650e44ac8571
Heaven31415 <Heaven31415@gmail.com>
2018-07-17 20:37:17 +0200

    Remove temporary menu.

--- app/state/menu.cr
+++ app/state/menu.cr
@@ -1,25 +1,13 @@
 require "./state.cr"
 require "./manager.cr"
 
-require "../gui/button.cr"
-require "../gui/cursor.cr"
-
 class Menu < State
   def initialize
     @cursor = Cursor.new
 
-    @settings = Button.new({200f32, 40f32})
-    @settings.position = {App.window.size.x * 0.5f32 - 100f32, App.window.size.y * 0.3f32}
-    @settings.add_label("Settings")
-
-    @exit = Button.new({200f32, 40f32})
-    @exit.position = {App.window.size.x * 0.5f32 - 100f32, App.window.size.y * 0.4f32}
-    @exit.add_label("Exit")
   end
 
   def draw(target : SF::RenderTarget)
-    target.draw(@settings)
-    target.draw(@exit)
     target.draw(@cursor)
   end
 
@@ -33,8 +21,6 @@ class Menu < State
       end
     end
 
-    @settings.handle_input(event)
-    @exit.handle_input(event)
     @cursor.handle_input(event)
   end
 

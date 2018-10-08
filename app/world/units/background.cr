0624a4c1e752a1bc2433259c916e5dbd69cd137c
Heaven31415 <Heaven31415@gmail.com>
2018-10-08 20:20:54 +0200

    Fix scrolling background rendering bug.

--- app/world/units/background.cr
+++ app/world/units/background.cr
@@ -15,17 +15,18 @@ class Background < Unit
     @extra = Unit.new(definition)
     @extra.default_transform
     @extra.velocity = App.config["BackgroundVelocity", SF::Vector2f]
-    @extra.move(0.0, -App.config["WindowHeight", Int32])
+    @extra.move(0.0, -App.window.size.y)
 
     self.default_transform
     self.velocity = App.config["BackgroundVelocity", SF::Vector2f]
   end
 
   def update(dt : SF::Time)
-    move(0.0, -2 * App.window.size.y) if position.y > App.window.size.y
-    @extra.move(0.0, -2 * App.window.size.y) if @extra.position.y > App.window.size.y
     @extra.update(dt)
     super
+
+    move(0.0, -2 * App.window.size.y) if position.y > App.window.size.y
+    @extra.move(0.0, -2 * App.window.size.y) if @extra.position.y > App.window.size.y
   end
 
   def draw(target, states)

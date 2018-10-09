8eb47876582a0a8c82201db16e9ecbbf080a4adf
Heaven31415 <Heaven31415@gmail.com>
2018-10-09 21:14:36 +0200

    Remove dead code.

--- common/utilities.cr
+++ common/utilities.cr
@@ -1,12 +1,5 @@
 require "openssl/sha1"
 
-class Pair(T1, T2)
-  property first, second
-
-  def initialize(@first : T1, @second : T2)
-  end
-end
-
 module Math
   def self.cartesian_to_polar(x : Number, y : Number)
     {radius: Math.hypot(x, y), angle: Math.atan2(y, x)}
